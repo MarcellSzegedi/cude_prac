c5b8ed05eb9a9c0c9b28e800dc6c1b08f2aaa204
Marcell Szegedi <szegedi.marcell@gmail.com>
2026-04-15 14:58:30 +0200

    small fix

--- task3/power_job_task3.sh
+++ task3/power_job_task3.sh
@@ -6,5 +6,5 @@ for block_dim in "${block_dims[@]}"
 do
   JOB_NAME="bdim_${block_dim}"
 
-  sbatch --job-name=$JOB_NAME ./task3/task3 $block_dim
+  sbatch --job-name=$JOB_NAME ./task3/run_task3.sh $block_dim
 done
