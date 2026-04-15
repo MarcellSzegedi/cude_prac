6dcfb3cd0f2d5986e14735fa67b83921df710206
Marcell Szegedi <szegedi.marcell@gmail.com>
2026-04-15 14:35:19 +0200

    added power job

---
+++ task3/power_job_task3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+block_dims = (16 32 64)
+
+for block_dim in "${block_dims[@]}"
+do
+  JOB_NAME="bdim_${block_dim}"
+
+  sbatch --job-name=$JOB_NAME ./task3/task3 $block_dim
+done
--- task3/run_task3.sh
+++ task3/run_task3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=task3
-#SBATCH --output=task3_output_%j.txt
-#SBATCH --error=task3_error_%j.txt
+#SBATCH --output=%j_output.txt
+#SBATCH --error=%j_error.txt
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -9,4 +8,6 @@
 #SBATCH --constraint="gpunode,TitanX"
 #SBATCH --gres=gpu:1
 
-./task3/task3
+BLOCK_DIM=$1
+
+./task3/task3 $BLOCK_DIM
